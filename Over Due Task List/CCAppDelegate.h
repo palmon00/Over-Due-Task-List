3c1a51258dbba61a02ea502a2f5b54299b5b85ae
Ray Louie <raylouie@Raymonds-MacBook-Air-2.local>
2014-03-17 20:23:42 -0700

    Initial Commit

---
+++ Over Due Task List/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Over Due Task List
+//
+//  Created by Raymond Louie on 3/17/14.
+//  Copyright (c) 2014 Raymond Louie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Over Due Task ListTests/Over_Due_Task_ListTests.m
@@ -0,0 +1,34 @@
+//
+//  Over_Due_Task_ListTests.m
+//  Over Due Task ListTests
+//
+//  Created by Raymond Louie on 3/17/14.
+//  Copyright (c) 2014 Raymond Louie. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Over_Due_Task_ListTests : XCTestCase
+
+@end
+
+@implementation Over_Due_Task_ListTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
